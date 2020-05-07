315983521528bbfdb5bb4a18aac427d28ae47a97
Francois Chauveau <francois.chauveau44@gmail.com>
2020-05-07 19:21:16 +0200

    Update A_ConnexionServlet.java
    a compléter !

--- src/main/java/foo/A_ConnexionServlet.java
+++ src/main/java/foo/A_ConnexionServlet.java
@@ -1,6 +1,7 @@
 package foo;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Entity;
+
+
 
 //With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
 @WebServlet(name = "UserConnexion", description = "Connexion de l'utilisateur", urlPatterns = "/connexion")
@@ -18,26 +26,6 @@ public class A_ConnexionServlet extends HttpServlet {
 	UserService userService = UserServiceFactory.getUserService();
 
 	String thisUrl, email, nom;
-
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-
-		thisUrl = req.getRequestURI();
-
-		resp.setContentType("text/html");
-		if (req.getUserPrincipal() != null) {
-			/*
-			 * getNicknameUser(); getEmailUser(); req.setAttribute("nom", nom);
-			 * this.getServletContext().getRequestDispatcher("/compteView.jsp").forward(req,
-			 * resp); req.setAttribute("email", email);
-			 * this.getServletContext().getRequestDispatcher("/compteView.jsp").forward(req,
-			 * resp);
-			 */
-			resp.sendRedirect("/view");
-		} else {
-			resp.sendRedirect(userService.createLoginURL(thisUrl));
-		}
-	}
 
 	public boolean getConnected() {
 		return userService.isUserLoggedIn();
@@ -53,6 +41,44 @@ public class A_ConnexionServlet extends HttpServlet {
 
 	public void getSignOut() {
 		userService.createLogoutURL(thisUrl);
+	}	
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+		thisUrl = req.getRequestURI();
+
+		resp.setContentType("text/html");
+		if (req.getUserPrincipal() != null) {		
+			/*
+			 * getNicknameUser(); getEmailUser(); req.setAttribute("nom", nom);
+			 * this.getServletContext().getRequestDispatcher("/compteView.jsp").forward(req,
+			 * resp); req.setAttribute("email", email);
+			 * this.getServletContext().getRequestDispatcher("/compteView.jsp").forward(req,
+			 * resp);
+			 */
+			resp.sendRedirect("/view");
+		} else {
+			
+			
+			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+			
+			//ici nickname +  email pour l'id
+			Entity user = new Entity("User", "gros test");
+			
+			//ici getNicknameUser()  pour le nickname
+			user.setProperty("nickname", "test");
+			
+			//ici getEmailUser()  pour l'email
+			user.setProperty("email", "test1");
+			
+			user.setProperty("creationDate", new Date());
+			
+			datastore.put(user);
+			
+			resp.sendRedirect(userService.createLoginURL(thisUrl));
+			
+		}
 	}
 
 }
